62fe114fda87e0dabbcf65ecb15798205b2fe66a
maicolpq31 <kevincanizales07@gmail.com>
2024-03-04 13:07:30 -0500

    1

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,5 +1,25 @@
 class Todo:
-    pass
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool =  False
+        self.tags: list[str] = []
+
+
+    def mark_completed(self) -> bool:
+        self.completed = True
+
+
+    def add_tag(self, tag: str):
+        if self.tag is not in self.tags:
+            self.tags.append[tag]
+
+
+    def __str__(self) -> str:
+        return f"code_ide {self.code_id}, title {self.title}"
+
+
 
 
 class TodoBook:
